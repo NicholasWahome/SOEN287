d4090d6ef176b232d7f7bddcfc12e193e3e2890c
Ren-cc <bigmoneyozzu@gmail.com>
2023-10-15 13:15:55 -0400

    added more to front page

--- scripts/visual-scripts.js
+++ scripts/visual-scripts.js
@@ -2,10 +2,16 @@
 function navDisplay(){
     var nav = document.getElementById("navbar");
     
+
+    
     if (nav.style.display === "block") {
         nav.style.display = "none";
+        document.querySelector(".hamburger").style.transform = `rotate(${0}deg)`;
+        
+        
     } else {
         nav.style.display = "block";
+        document.querySelector(".hamburger").style.transform = `rotate(${90}deg)`;
     }
 
 }
