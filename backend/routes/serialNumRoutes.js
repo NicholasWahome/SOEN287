ea6442bb01c3ee92fc2a2da3ce0f300f07e93f81
NicholasWahome <87381233+NicholasWahome@users.noreply.github.com>
2023-12-02 12:48:04 -0500

    Database Connection Established
    If you want I can add your IP address to connect directly to database

--- backend/routes/serialNumRoutes.js
+++ backend/routes/serialNumRoutes.js
@@ -4,10 +4,10 @@ const router = express.Router()
 // Database Configuration
 const oracledb = require('oracledb');
 const dbConfig = {
- user: 'admin',
- password: 'Database287//',
- connectString: "(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1521)(host=adb.ca-montreal-1.oraclecloud.com))(connect_data=(service_name=geed4444a402754_getsoftdatabase_medium.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))"
-}
+  user: 'admin',
+  password: 'Database287//',
+  connectString: "(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1521)(host=adb.ca-montreal-1.oraclecloud.com))(connect_data=(service_name=geed4444a402754_getsoftdatabase_medium.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))"
+ }
 
 
 // Create a serial number - route
